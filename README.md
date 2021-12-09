d2e16510eedf53f4669222e2186cf0b5bbfdd6e9
Carlos D Moron <81266641+carlosdmoron@users.noreply.github.com>
2021-12-09 15:21:00 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+# A Real State App
+This is a Next.js project bootstrapped with React.
+
+## Features
+Work in progress
+
+## How to run this app?
+
+First, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
---
+++ components/SearchFilters.jsx
@@ -0,0 +1,15 @@
+import {useEffect} from 'react';
+import {Flex, Box, Text, Icon, Input, Spinner, Button} from '@chakra-ui/react';
+import {useRouter} from 'next/router';
+import {MdCancel} from 'react-icons/md';
+import Image from 'next/image';
+
+const SearchFilters = () => {
+    return (
+       <div>
+           searchFilters
+       </div> 
+    )
+}
+
+export default SearchFilters
